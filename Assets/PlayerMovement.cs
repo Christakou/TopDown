3accce3d8debb24367cdbaa25d0b0b2bd5a2391f
Christakou <41486691+Christakou@users.noreply.github.com>
2020-10-02 15:52:23 +0100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 pointCoords;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.forward, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            pointCoords = cameraRay.GetPoint(rayLength);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + (movement.normalized * moveSpeed * Time.fixedDeltaTime));
+        Vector2 lookDirection = pointCoords - rb.position;
+        rb.rotation = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+    }
+}
